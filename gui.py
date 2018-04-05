527fc36b3aa4d89b257dc56edead184f36d47a4a
Cameron <cjmoto77@gmail.com>
2018-04-05 22:30:14 +1000

    minor changes

--- gui.py
+++ gui.py
@@ -6,11 +6,16 @@ root = Tk()
 
 root.wm_title("Discord Roflz Webhook Poster")
 
+class EventHandlers:
+
+    
+
+
 class ButtonCommands:
 
     def webhookPost():
-        webhook = webhook.Webhook(https://discordapp.com/api/webhooks/430687214549204993/KeRt_krZwouxhHNDrbkbg64HXgZdUM2XQlqL3Ny5SFusM-I-U79wbGaqqxy4UdF_kSB3, a.mbox_text, a.username_text, a.imgurl_text)
-        
+        webhook = webhook.Webhook("https://discordapp.com/api/webhooks/430687214549204993/KeRt_krZwouxhHNDrbkbg64HXgZdUM2XQlqL3Ny5SFusM-I-U79wbGaqqxy4UdF_kSB3", a.mbox_text, a.username_text, a.imgurl_text)
+
 
 
 class EntryForm:
@@ -31,7 +36,6 @@ class EntryForm:
         self.username_text = StringVar()
         self.entry1 = Entry(frameTop, textvariable=self.username_text)
         self.entry1.pack(side=LEFT, padx=2)
-        self.entry1.insert(END, "Roflz")
 
         self.label2 = Label(frameTop, text="Image Url:")
         self.label2.pack(side=LEFT)
@@ -42,7 +46,7 @@ class EntryForm:
 
         self.mbox = Text(frameBot, maxundo=10, height=10, width=50, wrap=WORD)
         self.mbox.pack(side=LEFT)
-        self.mbox_text = self.mbox.get(ANCHOR)
+        self.mbox_text = self.mbox.get(1.0, END)
 
         self.label3 = Label(frameLabe, text="Message:")
         self.label3.pack(side=LEFT)
@@ -54,7 +58,7 @@ class Buttons:
         frame = Frame(master)
         frame.grid(row=2, column=1, rowspan=2, padx=5, sticky=N, pady=10)
 
-        self.postButton = Button(frame, text="Post", width=12)
+        self.postButton = Button(frame, text="Post", width=12, command=ButtonCommands.webhookPost)
         self.postButton.pack(side=TOP)
 
         self.clearButton = Button(frame, text="Clear All", width=12)
